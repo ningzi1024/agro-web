d8db37bb775b68f3984a542897bc6073bf551a69
zelin <330302700@qq.com>
2019-04-23 01:22:17 +0800

    分类模型

---
+++ database/migrations/2019_04_18_163832_create_sorts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sorts', function (Blueprint $table) {
+            $table->increments('id')->autoIncrement();
+            $table->string('name');
+            $table->tinyInteger('parent_id')->nullable();
+            $table->string('desc')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sorts');
+    }
+}
